870b407fd1929aa7c6dcc322f1722abb843ee46a
TEAM ORCHID OS ROM <teamorchidos@gmail.com>
2020-12-20 17:57:56 +0000

    rebrand 1.2
    zip path Orchidos

--- build/envsetup.sh
+++ build/envsetup.sh
@@ -93,7 +93,7 @@ alias bib=breakfast
 function eat()
 {
     if [ "$OUT" ] ; then
-        ZIPPATH=`ls -tr "$OUT"/lineage-*.zip | tail -1`
+        ZIPPATH=`ls -tr "$OUT"/Orchidos-*.zip | tail -1`
         if [ ! -f $ZIPPATH ] ; then
             echo "Nothing to eat"
             return 1
