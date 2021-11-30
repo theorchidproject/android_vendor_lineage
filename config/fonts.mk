c9101202a1e63f719b29a7192fcbd16bbcb970b2
Omkar Chandorkar <gotenksIN@aosip.dev>
2021-11-30 08:35:49 +0100

    vendor: fonts: Import custom fonts
    * From https://github.com/yaap/vendor_themes/commit/b5502cc6b10f9cb3a61cd7f9778f8a30e62147a4
    * included fonts are Inter v3.19, Linotte, Manrope v4.504, OnePlus Sans and OnePlus Slate
    
    Change-Id: I57e29d75eb3ed27cb649eacffd30566314322b77
    
    Signed-off-by: Omkar Chandorkar <gotenksIN@aosip.dev>
    Signed-off-by: PainKiller3 <ninadpatil100@gmail.com>
    
    fonts
    
    Co-Authored-By: Jyotiraditya Panda <jyotiraditya@aospa.co>
    Co-Authored-By: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Co-Authored-By: Pranav Vashi <neobuddy89@gmail.com>
    Co-Authored-By: idoybh <idoybh2@gmail.com>

---
+++ config/fonts.mk
@@ -0,0 +1,13 @@
+# Fonts
+LOCAL_PATH := vendor/lineage/fonts
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,$(LOCAL_PATH)/ttf,$(TARGET_COPY_OUT_PRODUCT)/fonts)
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/fonts_customization.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/fonts_customization.xml
+
+PRODUCT_PACKAGES += \
+    FontInterOverlay \
+    FontLinotteSourceOverlay \
+    FontManropeOverlay \
+    FontOnePlusSansOverlay \
+    FontOneplusSlateSourceOverlay
