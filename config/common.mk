80bf46f7f1307815def2922816a8fe7aab3286ad
the projects <84352313+theorchidproject@users.noreply.github.com>
2022-04-11 23:28:45 +0100

    test build microg

--- config/common.mk
+++ config/common.mk
@@ -1,6 +1,6 @@
 # Allow vendor/extra to override any property by setting it first
 $(call inherit-product-if-exists, vendor/extra/product.mk)
-
+$(call inherit-product, vendor/microg/microg-vendor.mk)
 PRODUCT_BRAND ?= LineageOS
 
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
