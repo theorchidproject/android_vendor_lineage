54c53551394ff81da06997f27066e88b3f97e262
the projects <84352313+theorchidproject@users.noreply.github.com>
2022-05-29 05:55:42 +0100

    Removed gms,Tele,snotes

--- prebuilt/pepsi/config.mk
+++ prebuilt/pepsi/config.mk
@@ -1,20 +1,16 @@
 include $(call first-makefiles-under,$(LOCAL_PATH))
 
 PRODUCT_PACKAGES += \
-	Snotes \
     pdf \
     graphenecamera \
     material \
-    telegram \
     Openboard \
     dialer \
     pep \
     qksms \
     rootless \
     simpleGallery \
-    TapTap-v1.3 \
     org.microg.gms.droidguard-4 \
-    GmsCore \
     weather \
     audioR \
     warden
