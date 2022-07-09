9ec18be759aff93ac1f4d4ac058cdbbdf3ec2f08
theorchidproject <84352313+theorchidproject@users.noreply.github.com>
2022-07-09 17:04:22 +0100

    OrchidOs : Prebuilts : Orchid Aosp Foss install
    OrchidOs : Prebuilts :
    
    install for 12 rearrange a few aosp apks
    searched tried and tested
    fdroid or izzdroid apks clearance
    no ads clean source code no trackers
    privacy secure and light weight
    
    commercial version provides renumeration for app developers

---
+++ prebuilt/orchidos/config.mk
@@ -0,0 +1,17 @@
+PRODUCT_SOONG_NAMESPACES += \
+    vendor/lineage/prebuilt/orchidos
+
+PRODUCT_PACKAGES += \
+    audioR \
+    brair \
+    duckduck \
+    ForkClient \
+    graphenecamera \
+    material \
+    mnml \
+    music \
+    Openboard \
+    pdf \
+    pep \
+    qksms \
+    tap
