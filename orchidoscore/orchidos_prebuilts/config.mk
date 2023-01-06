c8ffc741f19c3f5f4f8a865cb61864f4bd0aa3fb
orchidromdev <orchidromdev@gmail.com>
2023-01-06 16:07:17 +0000

    Update config

--- orchidoscore/orchidos_prebuilts/config.mk
+++ orchidoscore/orchidos_prebuilts/config.mk
@@ -2,25 +2,16 @@ PRODUCT_SOONG_NAMESPACES += \
     vendor/lineage/orchidoscore/orchidos_prebuilts
 
 PRODUCT_PACKAGES += \
-    lawnfeed \
     audioR \
     sms \
     ForkClient \
 	k9mail \
-    gallery-375-foss-release.apk \
     Openboard \
     pdf \
     simplecam \
-	simplegallery \
 	tap \
     opencal \
     rethink \
-    webappssystem \
-    ameno \
-    tasks \
     adaway \
-    aegis \
     bitwarden \
-    brair \
-    delta \
     dns66 
