6825f66718ee05513ea84ccf3715e61af94fd351
theorchidproject <84352313+theorchidproject@users.noreply.github.com>
2023-01-24 15:56:03 +0000

    OrchidOs System Prebuilts : Application Update
    added new apps including overrides
    thank you fdroid, github and simple tools for the applications
        audioR, sms, ForkClient
        k9mail, pdf, simplecam
        opencal, rethinkdns
        adaway, bitwarden, dns66
        com.asdoi.quicktiles_14
        com.simplemobiletools.gallery.pro_380
        com.simplemobiletools.contacts.pro_100
        com.simplemobiletools.filemanager.pro_130
        io.github.domi04151309.alwayson_383
    
    Update Android.bp

---
+++ OrchidOsPrebuilts/config.mk
@@ -0,0 +1,22 @@
+PRODUCT_SOONG_NAMESPACES += \
+    vendor/lineage/orchidoscore/orchidos_prebuilts
+
+PRODUCT_PACKAGES += \
+    audioR \
+    sms \
+    ForkClient \
+	k9mail \
+    pdf \
+    simplecam \
+    opencal \
+    rethink \
+    adaway \
+    bitwarden \
+    dns66 \
+    com.asdoi.quicktiles_14 \
+    com.simplemobiletools.gallery.pro_380 \
+    com.simplemobiletools.contacts.pro_100 \
+    com.simplemobiletools.filemanager.pro_130 \
+    io.github.domi04151309.alwayson_383
+
+
