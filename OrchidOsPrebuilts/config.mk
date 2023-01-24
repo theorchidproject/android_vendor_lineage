0cba45f4230d631ac16765039517d15754ab3508
theorchidproject <84352313+theorchidproject@users.noreply.github.com>
2023-01-24 15:56:03 +0000

    OrchidOs Prebuilts : Import simple tools
    import simple tools in to the system oreveriding boring looking lineage apps updating to material you type design .
    al apps are open source thank you Simple tools

---
+++ OrchidOsPrebuilts/config.mk
@@ -0,0 +1,10 @@
+PRODUCT_SOONG_NAMESPACES += \
+    vendor/lineage/orchidos_prebuilts
+
+PRODUCT_PACKAGES += \
+    com.simplemobiletools.camera_84 \
+    com.simplemobiletools.contacts.pro_100 \
+    com.simplemobiletools.filemanager.pro_130 \
+    com.simplemobiletools.gallery.pro_380 \
+    com.simplemobiletools.smsmessenger_71
+
