4a6f563bd6c7e66c37c4815f5b69fdfb502d0e2e
the projects <84352313+theorchidproject@users.noreply.github.com>
2023-02-11 12:50:01 +0000

    OrchidOs : add coreapps FOSS
    add     audioR k9mail  pdf  opencal
      adaway  bitwarden dns66 io.github.domi04151309.alwayson_383  com.asdoi.quicktiles_14l

--- addons/OrchidOsPrebuilts/config.mk
+++ addons/OrchidOsPrebuilts/config.mk
@@ -6,5 +6,13 @@ PRODUCT_PACKAGES += \
     com.simplemobiletools.contacts.pro_100 \
     com.simplemobiletools.filemanager.pro_130 \
     com.simplemobiletools.gallery.pro_380 \
-    com.simplemobiletools.smsmessenger_71
-
+    com.simplemobiletools.smsmessenger_71 \
+    audioR \
+	k9mail \
+    pdf \
+    opencal \
+    adaway \
+    bitwarden \
+    dns66 \
+    io.github.domi04151309.alwayson_383 \
+    com.asdoi.quicktiles_14
