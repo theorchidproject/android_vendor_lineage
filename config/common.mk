5212c1b620be34ac2622defc7b5936b5c1fa2110
the projects <84352313+theorchidproject@users.noreply.github.com>
2023-02-11 14:12:54 +0000

    OrchidOs App Update : add Selfie and TapTap
    add lineage selfie cam
    add taptap
    remove simpletools cam

--- config/common.mk
+++ config/common.mk
@@ -4,6 +4,7 @@ $(call inherit-product-if-exists, vendor/extra/product.mk)
 $(call inherit-product-if-exists, vendor/lineage/bromite_system_inline/bromite.mk)
 $(call inherit-product-if-exists, vendor/lineage/lawnchair/lawnchair.mk)
 $(call inherit-product, vendor/lineage/addons/OrchidOsPrebuilts/config.mk)
+$(call inherit-product, vendor/lineage/addons/TapTap-v1.6.mk)
 $(call inherit-product, vendor/lineage/addons/addons.mk)
 $(call inherit-product, vendor/partner_gms/gms.mk)
 
